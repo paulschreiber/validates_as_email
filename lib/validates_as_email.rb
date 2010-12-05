03a70d4fcd93c43f4b5811f6e56335ae26cf8d30
Paul Schreiber <paulschreiber@gmail.com>
2010-12-05 15:50:16 -0500

    initial checkin

---
+++ lib/validates_as_email.rb
@@ -0,0 +1,25 @@
+module ActiveRecord
+  module Validations
+    module ClassMethods
+
+      EMAIL_ADDRESS_RE = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i
+      
+      def validates_as_email(*args)        
+        configuration = { :on => :save, :with => nil }
+        configuration.update(args.pop) if args.last.is_a?(Hash)
+        
+        validates_each(args, configuration) do |record, attr_name, value|
+          unless value =~ EMAIL_ADDRESS_RE
+            message = I18n.t("activerecord.errors.models.#{name.underscore}.attributes.#{attr_name}.invalid", 
+                                          :default => [:"activerecord.errors.models.#{name.underscore}.invalid", 
+                                                      configuration[:message],
+                                                      :'activerecord.errors.messages.invalid'])
+            record.errors.add(attr_name, message)
+          end
+        end
+      end
+
+
+    end    
+  end
+end
